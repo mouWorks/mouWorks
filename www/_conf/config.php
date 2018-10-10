897f5c0c2d359fe21515a19a6d023b2297156109
Unknown <mou.wang@104.com.tw>
2018-10-10 21:04:52 +0800

    Now using Controller Classes.

--- www/_conf/config.php
+++ www/_conf/config.php
@@ -9,5 +9,8 @@ define('SLIM_ROOT', __DIR__);
 return array(
     'debug' => true,
     'mode'  => 'development',
-    'templates.path' => __DIR__ . '/templates'
+    'templates.path' => __DIR__ . '/templates',
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
 );
