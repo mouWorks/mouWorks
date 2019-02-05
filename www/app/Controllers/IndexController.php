38fa8d94a1fea23b227371f9bba52d99b088ceca
MJ Wang <jimmyexx@gmail.com>
2019-02-06 01:32:24 +0800

    Add docker config and also add some content

--- www/app/Controllers/IndexController.php
+++ www/app/Controllers/IndexController.php
@@ -10,10 +10,11 @@ class IndexController
 
 	public function randWords()
 	{
-		$stuffArray = ['施工中', '崩崩中', 'G排中', 'QQ中', 'CD中', 'R6中'];
 
+		$stuffArray = ['施工中', '崩崩中', 'G排中', 'QQ中', 'CD中', 'R6中', 'PS中'];
 		$rand_result = array_rand($stuffArray, 1);
 
+		echo "Current Status: ";
 		echo $stuffArray[$rand_result];
 	}
 
