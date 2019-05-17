d1e8a6ef01b414a38009b55defd8b0b107529e3f
Mou <mou.wang@g.ncu.edu.tw>
2019-05-17 18:33:50 +0800

    Update layout, now seems possible to pass in Options. need more test

--- www/assets/js/poc-canvasFingerprint.js
+++ www/assets/js/poc-canvasFingerprint.js
@@ -1,39 +1,29 @@
 'use strict';
 var chokeString = '';
 
-
 //DOC Ready
 $( document ).ready(function() {
 
-    // $('h6 > .version').html('0011');
+    $('.version').html('0017 -with O');
 
     $('.get_print').on('click', function(){
 
-        // alert('getting your fingerprint');
+        var options =
+            {fonts: {extendedJsFonts: true}, excludes: {userAgent: true}
+        };
 
-        var options = {fonts: {extendedJsFonts: true}, excludes: {userAgent: true}}
+        Fingerprint2.get(options, function (components) {
 
-        Fingerprint2.get(function (components) {
             console.log(components) // an array of components: {key: ..., value: ...}
 
-            var murmur = Fingerprint2.x64hash128(components.map(function (pair) { return pair.value }).join(), 31)
+            var murmur = Fingerprint2.x64hash128(components.map(function (pair) { return pair.value }).join(), 200)
             mountResult(murmur);
 
+
         })
-
-        //Trigger the fingerprint
-        //mountResult('here it is!!')
-
     });
 
-
-    function mountResult(string)
-    {
+    function mountResult(string) {
         $('#result').text(string);
-
-    }
-
-    function checkTextType(){
-        return $('input[name=textOptions]:checked').attr('data-id');
     }
 });
