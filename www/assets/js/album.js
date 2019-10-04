d48199260423ad223f15e67f0ca8ac50d917da1d
mouWorks <mou.wang@g.ncu.edu.tw>
2019-10-04 17:32:53 +0800

    now it works

--- www/assets/js/album.js
+++ www/assets/js/album.js
@@ -1,51 +1,44 @@
 "use strict";
 
-
 var jasonPath = 'https://s3-us-west-2.amazonaws.com/q3buxya/WeddingHelper/L30/web/images.json';
 var prefix = 'https://s3-us-west-2.amazonaws.com/q3buxya/';
-
-$.ajax({
-    // beforeSend: function(request) {
-    //     request.setRequestHeader("Access-Control-Allow-Origin", '*');
-    // },
-    dataType: "json",
-    url: jasonPath,
-    success: function(data) {
-
-        var imgAlbum = '';
-        var result = data.forEach(function(item, index, array){
-
-            if(item.indexOf('.jpg') >= 0){
-                var currentImage = prefix + item;
-                imgAlbum += '<img src="' + currentImage + '" alt="Loading" height="75" width="75">';
-                // imgAlbum += '<a href="' + currentImage + '"><img class="lazy" data-src="' + currentImage + '" alt="Loading" height="100" width="100">';
-            }
-        });
-
-        //Mount
-        $(".images").after( imgAlbum );
-}
-});
 
 //toss in Object
 function showImg(thumbname){
 
-    var src_small = thumbname.src;
-    // var img_alt = thumbname.alt;
-    // var src_mid = get_diff_image_size(thumbname.src, 'M');
-    //show the Mid photo
-    $('.show').attr('src' , thumbname.src);
-    // $('.desc').html(img_alt);
+    var imageSource = thumbname.src;
+    $('.show').attr('src' , imageSource);
 }
 
+function bindImage(){
+    //Bind the event when clicked on a thumb
+    $('.image_thumbs>img').on('click', function (e) {
+        console.log('clicked');
+        showImg(this);
+    });
+}
 
 //DOM Ready
 $(function () {
+    $.ajax({
 
-    //Bind the event when clicked on a thumb
-    $('.image_thumbs>img').on('click', function (e) {
-        e.preventDefault();
-        showImg(this);
+        dataType: "json",
+        url: jasonPath,
+        success: function(data) {
+
+            var imgAlbum = '';
+            var result = data.forEach(function(item, index, array){
+
+                if(item.indexOf('.jpg') >= 0){
+                    var currentImage = prefix + item;
+                    imgAlbum += '<img src="' + currentImage + '" alt="Loading" height="75" width="75">';
+                }
+            });
+
+            //Mount
+            $(".images").after( imgAlbum );
+            bindImage();
+        }
     });
 });
 
