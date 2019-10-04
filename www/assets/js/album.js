4f30ad1fa8dac519076381eb31f3c27e46ccce87
mouWorks <mou.wang@g.ncu.edu.tw>
2019-10-04 17:41:44 +0800

    Update album

--- www/assets/js/album.js
+++ www/assets/js/album.js
@@ -40,5 +40,30 @@ $(function () {
             bindImage();
         }
     });
+
+    //Go to S3 to manage
+    $('#manage').on('click', function(){
+        swal(
+            {
+                type: "info",
+                title:"Feature still working...",
+                text: "(Close in 2s)",
+                timer: 2000
+            });
+
+        var s3url = 'https://s3.console.aws.amazon.com/s3/buckets/q3buxya/WeddingHelper/L30/photos/?region=ap-northeast-2';
+
+        //somehow this doesn't work right now
+        // setTimeout(function() {
+        //     swal({
+        //         type: "info",
+        //         title:"Heading to S3 bucket to manage...",
+        //         text: "(Close in 2s)",
+        //     }, function() {
+        //         window.location = "s3url";
+        //     });
+        // }, 2000);
+
+    })
 });
 
